7c99f494ae91354ec93c177b20419e7d233563da
LDH <ldh@example.com>
2023-01-14 18:36:37 +0800

    已完成

--- src/mock/mockServer.js
+++
@@ -1,7 +0,0 @@
-import Mock from 'mockjs'
-import banner from './banner.json'
-import floors from './floors.json'
-//模拟首页的大轮播图的服务器数据
-Mock.mock("/mock/banner", { code: 200, data: banner })
-//模拟floors组件轮播图的数据
-Mock.mock("/mock/floor",{code:200,data:floors})
--- src/utils/uuid.js
+++
@@ -1,9 +0,0 @@
-import { v4 as uuidv4 } from 'uuid'
-export default function () {
-  let uuid = localStorage.getItem('uuid');
-  if (!uuid) {
-    uuid = uuidv4()
-    localStorage.setItem('uuid',uuid)
-  }
-  return uuid
-}
